226d0281c22210e3e4894615939313d720ec9ed3
mcrice123 <mariacatherinerice@gmail.com>
2026-07-21 21:37:39 -0600

    First round of creating my own data charts with D3

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,31 @@
 import { Head } from '@inertiajs/react';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { dashboard } from '@/routes';
-import { MarsLineChart } from '@/components/mars-line-chart';
+import MarsBarChart from '@/components/mars-bar-chart';
+import MarsLineChart from '@/components/mars-line-chart';
+import * as d3 from 'd3';
+    const createTables = async () => {
+        let data = await             
+        d3.csv(url('static/linechart.json'))
+        let parseTime = d3.timeParse("%Y-%m-%d");
+        data.forEach((d) => {
+            d.date = parseTime(d.date);
+            d.close = +d.close;
+        });
+    }
+console.log("LINE DATA");
+const lineDataTables = createTables();
+console.log(lineDataTables);
+
+const data = [
+  { name: 'A', value: 30 },
+  { name: 'B', value: 80 },
+  { name: 'C', value: 45 },
+  { name: 'D', value: 60 },
+  { name: 'E', value: 20 },
+  { name: 'F', value: 90 },
+  { name: 'G', value: 50 },
+];
 
 export default function Dashboard() {
     return (
@@ -10,10 +34,20 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <MarsLineChart className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <MarsBarChart 
+                            data={data} 
+                            width={400} 
+                            height={220} 
+                            className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" 
+                            />
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <MarsLineChart
+                            data={lineDataTables}
+                            width={400}
+                            height={220}
+                            className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" 
+                            />
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
